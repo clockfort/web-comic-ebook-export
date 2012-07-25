7cd047f2d660fc02fa70c1eaf22ede1d7f913dd3
Chris Lockfort <clockfort@csh.rit.edu>
2012-07-25 07:11:41 -0400

    split line for readability

--- reorder.hs
+++ reorder.hs
@@ -9,6 +9,7 @@ main = do
 	handle <- openFile "requests" ReadMode
 	list <- hGetContents handle
 	let urls = filter (isInfixOf "http") $ splitOn " " list
-	let filenames = map concat $ map (take 2) $ map (split (onSublist ".jpg")) $ concat $ map (filter (isInfixOf ".jpg")) $ map (splitOn "/") urls
+	let fileSegments = concat $ map (filter (isInfixOf ".jpg")) $ map (splitOn "/") urls
+	let filenames = map concat $ map (take 2) $ map (split (onSublist ".jpg")) fileSegments
 	sequence_ [ renameFile filename (show (fromJust $ elemIndex filename filenames)++".jpg") | filename <- filenames ]
 	putStrLn "Done."
