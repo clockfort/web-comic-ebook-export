f514e9e035d414a6faf0568e3aa56de3c4977b89
Chris Lockfort <clockfort@redhat.com>
2012-07-25 21:08:46 -0400

    sexier code

--- reorder.hs
+++ reorder.hs
@@ -1,15 +1,20 @@
-import System.IO
+import Control.Monad
 import Data.List
 import Data.List.Split
-import Maybe
+import Data.Maybe
 import System.Directory
-import Control.Monad
+import System.IO
 
 main = do
 	handle <- openFile "requests" ReadMode
 	list <- hGetContents handle
 	let urls = filter (isInfixOf "http") $ splitOn " " list
-	let fileSegments = concat $ map (filter (isInfixOf ".jpg")) $ map (splitOn "/") urls
-	let filenames = map concat $ map (take 2) $ map (split (onSublist ".jpg")) fileSegments
+	let fileSegments = concatMap (filter (isInfixOf ".jpg") . splitOn "/") urls
+	let filenames = map ((concat . take 2) . split (onSublist ".jpg")) fileSegments
 	sequence_ [ renameFile filename (show (fromJust $ elemIndex filename filenames)++".jpg") | filename <- filenames ]
 	putStrLn "Done."
+
+lengthMax list = (length . show . last) list
+deltaDigit list = map ( (lengthMax list-) . length . show ) list
+
+
